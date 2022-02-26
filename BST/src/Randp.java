722ee4b55a991a16965026c4d71b8a86bb467ab6
neharishi <100495064+neharishi@users.noreply.github.com>
2022-02-26 14:48:42 -0800

    Update Randp.java

--- BST/src/Randp.java
+++ BST/src/Randp.java
@@ -41,50 +41,5 @@ public class Randp {
    }
 }
 
-//public class RandP {
-//	private int counter;
-//	private int[] nums;
-//	
-//	public RandP(int n) {
-//		nums = new int[n];
-//		initNums(nums);
-//		counter = n;
-//	}
-//
-//	//Fills an array with consecutive numbers from 1 to nums.length
-//	public void initNums(int[] nums) {
-//		for (int i = 0; i < nums.length; i++) {
-//			nums[i] = i+1;
-//		}
-//	}
-//	
-//	public int size() {
-//		return nums.length;
-//	}
-//
-//	public void swap(int[] arr, int a, int b) {
-//		int temp = arr[a];
-//		arr[a] = arr[b];
-//		arr[b] = temp;
-//	}
-//	
-//	public int nextInt() {
-//		if (counter == 0) return 0;
-//		
-//		//selects a random index number to be used in the array
-//		int position = (int) (counter * Math.random());
-//		
-//		//decrease the counter so that it is the last index in the array
-//		counter--;
-//		
-//		//get the return value 
-//		int returnValue = nums[position];
-//		
-//		//swap the numbers at nums[position] and nums[counter]
-//		swap(nums, position, counter);
-//		
-//		//return the value
-//		return returnValue;
-//	}
-//}
+
 
